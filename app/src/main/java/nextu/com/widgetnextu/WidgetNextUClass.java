7b30d314f851ce2b038926e1cfd56deb3a2b3c13
omarking <quimipedro@yahoo.com>
2019-07-25 05:15:13 -0500

    25-07-2019 / 5:14

---
+++ app/src/main/java/nextu/com/widgetnextu/WidgetNextUClass.java
@@ -0,0 +1,21 @@
+package nextu.com.widgetnextu;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class WidgetNextUClass extends AppWidgetProvider {
+
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+
+    }
+
+}
